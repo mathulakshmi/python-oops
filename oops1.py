f4973313d790119fd08305a4ffccf108c11272e8
mathulakshmi <mathulakshmi112@gmail.com>
2023-04-08 04:48:41 +0530

    oops1.py

---
+++ oops1.py
@@ -0,0 +1,19 @@
+
+class student():    
+   
+   def __init__(self,name,age):
+      self.name=name
+      self.age=age
+   def printdetails(self):
+
+      return f"name is {self.name}, age is {self.age}"
+
+Mathu=student("Mathu",27)
+Saranya=student("Saranya",21)
+
+
+               
+print(Mathu.age)
+print(Saranya.name)
+print(Saranya.age)
+
